ff59e1537bc2610b32d9d29f8400d5b987a8103c
Deepesh Dandriyal <deepeshdandriyal1@gmail.com>
2023-08-26 13:11:54 +0530

    some visual changes only

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div className="px-4 py-12 max-w-2xl mx-auto">
+    <div className="px-4 py-9 max-w-2xl mx-auto">
       <h1 className="text-3xl font-bold mb-4 text-slate-800 text-center">
         About
       </h1>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="px-4 py-12 max-w-2xl mx-auto">
+    <div className="px-4 py-9 max-w-2xl mx-auto">
       <h1 className="text-3xl font-bold mb-4 text-slate-800 text-center">
         Welcome to my authentication app!!
       </h1>
