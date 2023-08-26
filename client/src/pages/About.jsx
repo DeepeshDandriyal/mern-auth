e3e6b528afe60185a71efe1ceb883f92cb8e4027
Deepesh Dandriyal <deepeshdandriyal1@gmail.com>
2023-08-26 12:12:43 +0530

    home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,29 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800 text-center">
+        About
+      </h1>
+      <p className="mb-3 text-slate-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia minus
+        aliquid amet expedita rem commodi sunt perferendis in dolore, facilis
+        voluptas molestiae repudiandae veritatis nesciunt omnis repellendus
+        illum voluptates iusto?
+      </p>
+      <p className="mb-3 text-slate-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia minus
+        aliquid amet expedita rem commodi sunt perferendis in dolore, facilis
+        voluptas molestiae repudiandae veritatis nesciunt omnis repellendus
+        illum voluptates iusto?
+      </p>
+      <p className="mb-3 text-slate-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia minus
+        aliquid amet expedita rem commodi sunt perferendis in dolore, facilis
+        voluptas molestiae repudiandae veritatis nesciunt omnis repellendus
+        illum voluptates iusto?
+      </p>
+    </div>
+  );
 };
 
 export default About;
